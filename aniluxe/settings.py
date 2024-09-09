fd04e5a798cea37fc257b03dacb2de69d14cc41c
ramim141 <ahramu584@.com>
2024-09-10 01:44:34 +0600

    csrf  added

--- aniluxe/settings.py
+++ aniluxe/settings.py
@@ -62,8 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
+
 
 ROOT_URLCONF = 'aniluxe.urls'
 
